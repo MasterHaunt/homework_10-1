394f4b6c593d2335274f43d5309596eab3a7a765
Andrey Cheltyomov <master.haunt@gmail.com>
2024-06-16 21:07:25 +0700

    homework_10-1 done

--- src/masks.py
+++ src/masks.py
@@ -8,7 +8,7 @@ def get_mask_card_number(card_number: str) -> str:
 
 
 def get_mask_account(account_number: str) -> str:
-    """Функция маскировки номера банковской карты"""
+    """Функция маскировки номера банковского счёта"""
     if account_number.isdigit() and len(account_number) == 20:
         masked_account = "**" + account_number[16:]
         return masked_account
