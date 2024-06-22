1770836d0ae81ee8e7aa7c285ce08aa583cc1079
Andrey Cheltyomov <master.haunt@gmail.com>
2024-06-23 01:25:33 +0700

    homework 11-1 done - fixed

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,6 +2,8 @@ from src import generators
 
 
 def test_card_number_generator():
+    """Тестирование функции-генератора номеров банковских карт. 'start_index' (начальный номер) = 100,
+    'stop_index (конечный номер) = 105"""
     card_numbers = generators.card_number_generator(100, 105)
     assert next(card_numbers) == "0000 0000 0000 0100"
     assert next(card_numbers) == "0000 0000 0000 0101"
@@ -12,6 +14,10 @@ def test_card_number_generator():
 
 
 def test_filter_by_currency(adv_transactions_list):
+    """Тестирование функции-генератора фильтрации транзакций по заданной валюте.
+    Использует фикстуру 'adv_transactions_list' из модуля 'conftest.py'
+    Инициализируется два итератора: 'rub_transactions' - для транзакций проведённых в рублях ("RUB"),
+    'usd_transactions'  - для транзакций проведённых в долларах США ('USD')"""
     rub_transactions = generators.filter_by_currency(adv_transactions_list, "RUB")
     assert next(rub_transactions)["id"] == 873106923
     assert next(rub_transactions)["id"] == 594226727
@@ -22,6 +28,8 @@ def test_filter_by_currency(adv_transactions_list):
 
 
 def test_transaction_descriptions(adv_transactions_list):
+    """Тестирование функции-генератора описаний транзакций. Использует фикстуру 'adv_transactions_list'
+    из модуля 'conftest.py'"""
     transactions_descriptions = generators.transaction_descriptions(adv_transactions_list)
     assert next(transactions_descriptions) == "Перевод организации"
     assert next(transactions_descriptions) == "Перевод со счета на счет"
